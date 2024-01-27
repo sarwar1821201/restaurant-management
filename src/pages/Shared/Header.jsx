ff11b6548e86263ea42219677e38526036e693e0
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-28 02:29:56 +0600

    set banner image

--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
 
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-30 bg-black max-w-screen-xl text-white bg-base-100">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
