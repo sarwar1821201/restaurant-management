22983304f2b4925b01bbe239810e084808656baf
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-28 01:49:25 +0600

    initall react router,tailwind and set footer route

---
+++ src/layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            
+          <Outlet></Outlet>
+          <Footer></Footer>
+
+        </div>
+    );
+};
+
+export default Main;
