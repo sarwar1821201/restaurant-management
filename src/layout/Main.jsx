264bdae80d7d06b51c4b8f7c1102420c76bfbca8
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-28 02:03:26 +0600

    set header

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/Shared/Footer';
+import Header from '../pages/Shared/Header';
 
 const Main = () => {
     return (
         <div>
-            
+           <Header></Header> 
           <Outlet></Outlet>
           <Footer></Footer>
 
