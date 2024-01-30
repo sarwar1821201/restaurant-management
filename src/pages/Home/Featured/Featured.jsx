f05290271ede806d1e0456a02f9805bcdbfa9548
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-30 23:12:04 +0600

    set background featured image

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import './Featured.css'
+import featuredIage from '../../../assets/home/featured.jpg'
+
+const Featured = () => {
+    return (
+        <div className='featured-item bg-fixed text-white pt-10 my-10 bg-slate-500 bg-opacity-60 ' >
+            <SectionTitle 
+              heading='Featured Item'
+              subHeading='Check It Out'
+            >
+
+            </SectionTitle>
+
+            <div className='md:flex justify-center items-center px-36 pb-20 pt-12 ' >
+                <div>
+                    <img src={featuredIage} alt="" />
+                </div>
+                <div className='md:ml-10' >
+                    <p>Aug 20, 2029</p>
+                    <p className='uppercase' > Where can i get some?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error non explicabo necessitatibus id distinctio quisquam sapiente iure. Animi reiciendis architecto accusamus voluptatum dolorum rerum, quos consectetur quidem doloremque, aliquam sint accusantium? Explicabo quae unde exercitationem, quis ipsa porro sed vero esse. Corrupti alias, nulla aperiam cumque dolor minus natus praesentium?</p>
+                    <button className="btn btn-outline">Order Now</button>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Featured;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import Categoty from '../Category/Categoty';
 import PopularMenu from '../PopularMenu/PopularMenu';
+import Featured from '../Featured/Featured';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Categoty></Categoty>
             <PopularMenu></PopularMenu>
+            <Featured></Featured>
         </div>
     );
 };
