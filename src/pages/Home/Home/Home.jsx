bae000670820582f9ab34e675a9e65740906cbc8
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-30 22:23:04 +0600

    show popular menu items in the home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Categoty from '../Category/Categoty';
+import PopularMenu from '../PopularMenu/PopularMenu';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Categoty></Categoty>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
