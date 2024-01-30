5a50d18728a2f323037cbf7cdeaeaa2c89f94414
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-30 18:02:32 +0600

    add swipe carusoul

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Categoty from '../Category/Categoty';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Categoty></Categoty>
         </div>
     );
 };
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn">Login</a>
   </div>
 </div>
         </div>
