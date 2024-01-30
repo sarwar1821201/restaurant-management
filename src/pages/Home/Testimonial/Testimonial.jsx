1e4a275333a23520a48232e258a152ff3f2a0f31
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-31 00:22:52 +0600

    set dynamic ratings

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -4,11 +4,14 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { Navigation } from 'swiper/modules';
+import '@smastrom/react-rating/style.css'
+
 
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { Rating } from '@smastrom/react-rating';
 
 const Testimonial = () => {
  
@@ -34,8 +37,15 @@ const Testimonial = () => {
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
          {
             reviews.map (review =>  <SwiperSlide key={review._id} >
-                <div className='m-10' >
-                    <p>{review.details}</p>
+                <div className='m-10 flex flex-col items-center mx-24 my-16  ' >
+ 
+                <Rating
+      style={{ maxWidth: 180 }}
+      value={review.rating}
+      readOnly
+    />
+
+                    <p className='py-8' >{review.details}</p>
                     <h2 className='text-2xl text-orange-400 text-center ' >{review.name}</h2>
                 </div>
                 </SwiperSlide>
