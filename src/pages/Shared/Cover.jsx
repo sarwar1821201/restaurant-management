7f0d82f15ef1a11e2d67dd3d34b352e7a0ea4613
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-02-01 00:01:46 +0600

    implement cover with blur paralex

--- src/pages/Shared/Cover.jsx
+++ src/pages/Shared/Cover.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
+
 
 const Cover = ({img, title }) => {
     return (
         <div>
-            <div className="hero h-[500px]" style={{backgroundImage: `url( "${img}" )`}}>
+      
+      <Parallax
+        blur={{ min: -50, max: 50 }}
+        bgImage={img}
+        bgImageAlt="the menu"
+        strength={-200}
+    >
+
+<div className="hero h-[500px]" >
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
@@ -12,6 +22,11 @@ const Cover = ({img, title }) => {
     </div>
   </div>
 </div>
+
+    </Parallax>
+
+
+     
         </div>
     );
 };
