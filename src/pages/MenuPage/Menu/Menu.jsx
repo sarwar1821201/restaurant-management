3647e6eb912e8700c42e92304ca1aafcde146f78
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-01-31 23:10:19 +0600

    set our menu page

--- src/pages/MenuPage/Menu/Menu.jsx
+++ src/pages/MenuPage/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover';
+import menuImg from '../../../assets/menu/banner3.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 
 const Menu = () => {
@@ -8,6 +11,14 @@ const Menu = () => {
             <Helmet>
                 <title>Our | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"  ></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover img={menuImg} title="our menu"  ></Cover>
+            <PopularMenu></PopularMenu>
+
+            <Cover img={menuImg} title="our menu"  ></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
