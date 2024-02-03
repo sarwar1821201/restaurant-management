218ad8abe6a5c5e217eefccad25790d34ee8be8e
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-02-03 17:51:39 +0600

    set authstatechange

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,19 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Router.jsx'
 
 import {  HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-    <HelmetProvider>
+   <AuthProvider>
+   <HelmetProvider>
     <div className='max-w-screen-xl mx-auto' >
     <RouterProvider router={router}  ></RouterProvider>
     </div>
     </HelmetProvider>
+   </AuthProvider>
     
   </React.StrictMode>,
 )
