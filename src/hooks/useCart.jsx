eb22de78712e97cce6486aa85ca4b79008eaa685
sarwar1821201 <sarwardipo1821201@gmail.com>
2024-02-12 14:29:10 +0600

    use react query and load data

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,5 +1,20 @@
-  
-  const useCart = (email) => {
+import { useQuery } from '@tanstack/react-query'
+import { useContext } from 'react';
+import { AuthContext } from '../providers/AuthProvider';
+
+  const useCart = () => {
+     const {user} = useContext(AuthContext)
+
+     const { isPending, refetch, isError, data : cart=[] } = useQuery({
+        queryKey: ['carts', user?.email ],
+        queryFn: async ()=> {
+            const response = await fetch(`http://localhost:5000/carts?email=${user.email}`)
+
+            return response.json()
+
+        } ,
+      })
+    return [cart, isPending]
 
   }
 
--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,31 @@ import { router } from './routes/Router.jsx'
 import {  HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './providers/AuthProvider.jsx'
 
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { getTodos, postTodo } from '../my-api'
+
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
    <AuthProvider>
    <HelmetProvider>
-    <div className='max-w-screen-xl mx-auto' >
+
+   <QueryClientProvider client={queryClient}>
+   <div className='max-w-screen-xl mx-auto' >
     <RouterProvider router={router}  ></RouterProvider>
     </div>
+    </QueryClientProvider>
+
+   
     </HelmetProvider>
    </AuthProvider>
     
